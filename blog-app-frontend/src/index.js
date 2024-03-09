ed2b427900bdead47d9f50a89e0ea4575ed80c2a
Vikas Rai <61798248+Raisahab77@users.noreply.github.com>
2024-03-09 23:56:32 +0530

    Update index.js

--- blog-app-frontend/src/index.js
+++ blog-app-frontend/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Added a new comment
 root.render(
   <React.StrictMode>
     <App />
